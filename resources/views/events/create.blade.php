918e71dd545aca16dbd9b2caff54285857097565
Jonas Santos <68215037+JonasHenriqueDev@users.noreply.github.com>
2023-05-02 01:30:27 -0300

    criação do formulario

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,18 @@
 @section('title', 'Produto')
 
 @section('content')
-    <h1>Criar evento</h1>
+    <div id="formulario">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST">
+            Evento: <br><input type="text" name="title"><br>
+            Cidade: <br><input type="text" name="city"><br>
+            O evento é privado? <br><select type="radio" name="private">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select><br>
+            Descrição: <br>
+            <textarea type="text" name="description" rows="5" cols="55"></textarea><br>
+            <input type="submit" value="Criar Evento">
+        </form>
+    </div>
 @endsection
